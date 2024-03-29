8a29edb5eedf927ecb9139b3b4760dea3fcc6a87
Hikaru Wada <watyaa@gmail.com>
2024-03-29 21:47:40 +0900

    Update packages

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,14 +2,16 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/scrapbox/[slug].ts";
+import * as $api_scrapbox_slug_ from "./routes/api/scrapbox/[slug].ts";
+
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/api/scrapbox/[slug].ts": $0,
+    "./routes/api/scrapbox/[slug].ts": $api_scrapbox_slug_,
   },
   islands: {},
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
